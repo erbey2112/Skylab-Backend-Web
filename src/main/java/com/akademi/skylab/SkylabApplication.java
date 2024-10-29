7aa8066fa2966e464e4b5a5a2c107dba168a82b0
Eren Özdemir <eren.ozdemir2005123@gmail.com>
2024-10-29 14:43:50 +0300

    Readme changed again

---
+++ src/main/java/com/akademi/skylab/SkylabApplication.java
@@ -0,0 +1,15 @@
+package com.akademi.skylab;
+
+import com.akademi.skylab.entities.Restaurant;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkylabApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/akademi/skylab/SkylabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akademi.skylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkylabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
